d5b158e3f1941c286e6affb4ba9f778d9233ed83
Phelipe Melo Matos <70668772+phelipee15@users.noreply.github.com>
2024-02-28 11:04:59 -0300

    Add files via upload

--- arvoreb.c
+++ arvoreb.c
@@ -204,180 +204,160 @@ Arvb *pesquisa(Arvb *arv, int valor){ // Retorna a Árvore(Página) onde se enco
 
 #pragma region Funções de Remoção
 
-void adicionarPai(Arvb *pai,Arvb *filho,int local){
-
-    /*
-    essa funcao remove da pagina pai e adiciona na pagina filha 
-    */
-    filho->itens[filho->tamanho++]=pai->itens[local];
-
-    for(int i=local;i<pai->tamanho-1;i++){//
-        pai->itens[i]=pai->itens[i+1];
-        pai->filhos[i+1]=pai->filhos[i+2];
+void adicionarPai(Arvb *pai, Arvb *filho, int local) { // Remove os itens da Página pai e insere na Página filha
+    filho->itens[filho->tamanho++] = pai->itens[local];
+    for (int i = local; i < pai->tamanho-1; i++) {
+        pai->itens[i] = pai->itens[i+1];
+        pai->filhos[i+1] = pai->filhos[i+2];
     }
     pai->tamanho--;
 }
 
-int suporta(Arvb *a,Arvb *b){//testa se suporta adicionar uma pagina b na pagina a e um item da pagina pai
-    return tamanho(a)+tamanho(b)<ORDEM-2;
+int suporta(Arvb *a, Arvb *b){ // Retorna se suporta adicionar uma Página B na Página A e um item da Página pai
+    return tamanho(a) + tamanho(b) < ORDEM-2;
 }
 
-int metade(Arvb *arv){//testa se está abaixo da capacidade minima
-    return arv->tamanho<METADE;
+int metade(Arvb *arv){ // Retorna se está abaixo da capacidade mínima
+    return arv->tamanho < METADE;
 }
 
-void unir(Arvb *a,Arvb *b){
-    /*
-        essa funcão une a pagina A com a B e apaga a B 
-    */
+void unir(Arvb *a,Arvb *b){ // Une a Página A com a B e remove a B
     printf("iniciada a uniao\n");
-    if(vaziaOrdem(a) || vaziaOrdem(b)){
+    if (vaziaOrdem(a) || vaziaOrdem(b)) {
         printf("nao pode unir\n");
         exit(0);
     }
-    int i=a->tamanho,j=0;
-    for(;i<(a->tamanho+b->tamanho+1);i++){
-        a->itens[i]=b->itens[j];
-        a->filhos[i]=b->filhos[j++];
+    int i = a->tamanho, j = 0;
+    for(; i<(a->tamanho + b->tamanho + 1); i++) {
+        a->itens[i] = b->itens[j];
+        a->filhos[i] = b->filhos[j++];
     }
-    a->tamanho+=b->tamanho;
-
-    b->tamanho=0;
+    a->tamanho += b->tamanho;
+    b->tamanho = 0;
 }
 
-void underSplit(Arvb *arv,Arvb *pai){//testa e executa o movimento inverso do split
-
-    int local=numeroFilho(pai,arv);//descobre qual a localizaçao do filho na pagina pai
-
-    if(local<tamanho(pai)-1 && local!=0){
+void underSplit(Arvb *arv, Arvb *pai) { // Faz o inverso do split
+    int local = numeroFilho(pai, arv); // Descobre a localização do filho na Página pai
+    if (local < tamanho(pai)-1 && local != 0) {
         /*verifica se de acordo com a posicão a pagina vai ter 
         vizinhos tanto na esquerda quanto na direita,caso seja maior que 0
         e menor do que o tamanho ela possui dos dois vizinhos*/
-        if(suporta(arv,pai->filhos[local-1])){
-            Arvb *aux=pai->filhos[local-1];
-            adicionarPai(pai,pai->filhos[local-1],local-1);
-            unir(aux,arv);
-            if(vazia(raiz)){//caso a raiz fique vazia muda a raiz para o filho
+        if (suporta(arv, pai->filhos[local-1])) {
+            Arvb *aux = pai->filhos[local-1];
+            adicionarPai(pai, pai->filhos[local-1], local-1);
+            unir(aux, arv);
+            if (vazia(raiz)) {//caso a raiz fique vazia muda a raiz para o filho
                 free(raiz);
-                raiz=aux;
+                raiz = aux;
             }
         }
-        if(suporta(arv,pai->filhos[local+1])){
-            Arvb *aux=pai->filhos[local+1];
-            adicionarPai(pai,arv,local);
-            unir(arv,aux);
-            
-            if(vazia(raiz)){
+        if (suporta(arv, pai->filhos[local+1])) {
+            Arvb *aux = pai->filhos[local+1];
+            adicionarPai(pai, arv, local);
+            unir(arv, aux);
+            if (vazia(raiz)) {
                 free(raiz);
-                raiz=arv;
+                raiz = arv;
             }
         }
     }
-    else if(local==0){//caso seja o 0 so possui o vizinho da direita
-        if(suporta(arv,pai->filhos[local+1])){
-            Arvb *aux=pai->filhos[local+1];
-            adicionarPai(pai,arv,local);
-            unir(arv,aux);
-            if(cheia(arv))
-                split(arv,pai);
+    else if (local == 0) { // Caso seja o 0, só possui o vizinho da direita
+        if (suporta(arv,pai->filhos[local+1])) {
+            Arvb *aux = pai->filhos[local+1];
+            adicionarPai(pai, arv, local);
+            unir(arv, aux);
+            if (cheia(arv))
+                split(arv, pai);
             }
-            if(vazia(raiz)){
+            if (vazia(raiz)){
                 free(raiz);
-                raiz=arv;
+                raiz = arv;
             }
-    }else{//caso seja o ultimo so possui o vizinho da esquerda
-        if(suporta(arv,pai->filhos[local-1])){
-            Arvb *aux=pai->filhos[local-1];
-            adicionarPai(pai,pai->filhos[local-1],local-1);
-            unir(aux,arv);
-           
-            if(vazia(raiz)){
+    } else { // Caso seja o ultimo so possui o vizinho da esquerda
+        if (suporta(arv, pai->filhos[local-1])) {
+            Arvb *aux = pai->filhos[local-1];
+            adicionarPai(pai, pai->filhos[local-1], local-1);
+            unir(aux, arv);
+            if (vazia(raiz)) {
                 free(raiz);
-                raiz=aux;
+                raiz = aux;
             }
         }
     }
     
 }
 
-void substituir(Arvb *arv,int *valor,Arvb *pai){
-    /*caso precise apagar um item de uma pagina que possua filho ela vai
-    substituir o item com o numero mais proximo*/
-
-    if(vaziaOrdem(arv->filhos[0])){
-        swap(&(arv->itens[arv->tamanho-1]),valor);
+void substituir(Arvb *arv, int *valor, Arvb *pai) { // Caso precise apagar um item de uma Página que possua filhos, vai substituir o item com número mais próximo(sucessor)
+    if (vaziaOrdem(arv->filhos[0])) {
+        swap(&(arv->itens[arv->tamanho-1]), valor);
         arv->tamanho--;
-    }else{
-        substituir(arv->filhos[arv->tamanho],valor,arv);
+    } else {
+        substituir(arv->filhos[arv->tamanho], valor, arv);
     }
-    if(metade(arv))
-        underSplit(arv,pai);
-
+    if (metade(arv))
+        underSplit(arv, pai);
 }
 
-int removerPagina(Arvb *arv,int valor){//remove o item da pagina
-
-    int status=0;
-    if(vaziaOrdem(arv->filhos[0])){
-        for(int i=0;i<arv->tamanho;i++){
-            if(arv->itens[i]==valor){//faz o item ir para o final da pagina e diminui o tamanho dela
-                swap(&(arv->itens[i]),&(arv->itens[i+1]));
-                status=1;
+int removerPagina(Arvb *arv, int valor){ // Remove o item da Página
+    int status = 0;
+    if (vaziaOrdem(arv->filhos[0])) {
+        for (int i = 0; i < arv->tamanho; i++) {
+            if (arv->itens[i] == valor){ // Coloca o item no final da Página e diminui o tamanho dela
+                swap(&(arv->itens[i]), &(arv->itens[i+1]));
+                status = 1;
             }
         }
     }
-    if(status==1)
+    if (status == 1)
         arv->tamanho--;
     return status;
 }
 
-void removerArv(Arvb *arv,Arvb *pai,int valor){//remocão geral
-    if(vaziaOrdem(arv)){
+void removerArv(Arvb *arv, Arvb *pai, int valor){ // Remover ...
+    if (vaziaOrdem(arv)) {
         printf("arvore nao inicida ainda\n");
-    }else{
-        if(vazia(arv)){
+    } else {
+        if (vazia(arv)) {
             printf("nao possui elementos para remover\n");
-        }else{
-           if(vaziaOrdem(arv->filhos[0])){//caso ela nao possua filhos
-               removerPagina(arv,valor)? printf("removeu\n"):printf("nao existe\n");
-           }else{
-               for(int i=0;i<arv->tamanho;i++){
-                   if(valor==arv->itens[i]){//executa a funcao de troca
-                        substituir(arv->filhos[i],&(arv->itens[i]),arv);
+        } else {
+            if (vaziaOrdem(arv->filhos[0])) { //caso ela nao possua filhos
+               removerPagina(arv, valor) ? printf("removeu\n"):printf("nao existe\n");
+            } else {
+                for (int i = 0; i < arv->tamanho; i++) {
+                    if(valor == arv->itens[i]){ //executa a funcao de troca
+                        substituir(arv->filhos[i], &(arv->itens[i]), arv);
                         break;
-                   }else if(valor<arv->itens[i]){
-                        removerArv(arv->filhos[i],arv,valor);
+                    } else if (valor < arv->itens[i]) {
+                        removerArv(arv->filhos[i], arv, valor);
                         break;
-                   }else if(i==arv->tamanho-1){
-                        removerArv(arv->filhos[i+1],arv,valor);
+                    } else if (i == arv->tamanho-1) {
+                        removerArv(arv->filhos[i+1], arv, valor);
                         break;
-                   }
+                    }
                }
-           }
-
-           if(metade(arv) && arv!=raiz && vazia(arv)!=1){//testa se esta da capacidade minima
-                underSplit(arv,pai);
-           }
+            }
+            if (metade(arv) && arv != raiz && vazia(arv) !=1 ) { //testa se esta da capacidade minima
+                underSplit(arv, pai);
+            }
         }
     }
 }
 
-////Apagar tudo
-void apagar(Arvb *arv){
-    if(vaziaOrdem(arv)){
+void apagar(Arvb *arv){ // Apagar toda a Árvore
+    if (vaziaOrdem(arv)) {
         printf("nao iniciada\n");
-    }else{
-        if(vazia(arv)){
+    } else {
+        if (vazia(arv)) {
             printf("nao possue elementos\n");
-        }else{
-            if(vaziaOrdem(arv->filhos[0])){
+        } else {
+            if (vaziaOrdem(arv->filhos[0])) {
                 free(arv);
-            }else{
-                for(int i=0;i<arv->tamanho;i++){
+            } else {
+                for (int i = 0; i < arv->tamanho; i++) {
                     apagar(arv->filhos[i]);
                 }
-                if(arv==raiz)
-                    arv->tamanho=0;
+                if(arv == raiz)
+                    arv->tamanho = 0;
                 else
                     free(arv);
             }
@@ -389,48 +369,46 @@ void apagar(Arvb *arv){
 
 #pragma region Funções de Imprimir
 
-void printLista(int *list,int tamanho){
-    for(int i=0;i<tamanho;i++){
-            printf("%d ",list[i]);
+void printLista(int *list, int tamanho){ // Imprime uma Página
+    for (int i = 0; i < tamanho; i++){
+        printf("%d ", list[i]);
     }
 }
 
-void printArv(Arvb *arv,int altura){
-    if(vaziaOrdem(arv)){
+void printArv(Arvb *arv, int altura) { // Imprime a Árvore toda
+    if (vaziaOrdem(arv)) {
         printf("nao iniciada a arvore\n");
-    }else{
-        if(vazia(arv)){
+    } else {
+        if (vazia(arv)) {
             printf("vazia\n");
-        }else{
-            if(arv==raiz)
-                printf("raiz%*s",10,"");
+        } else {
+            if(arv == raiz)
+                printf("raiz%*s", 10, "");
             else
-                printf("filho[%d]%*s",altura,altura*3 +10,"|");
-            
-            printLista(arv->itens,arv->tamanho);
+                printf("filho[%d]%*s", altura, altura*3 +10, "|");
+            printLista(arv->itens, arv->tamanho);
             printf("\n");
-
-            if(vaziaOrdem(arv->filhos[0])==0)
-                for(int i=0;i<=arv->tamanho;i++){
-                    printArv(arv->filhos[i],altura+1);
+            if(vaziaOrdem(arv->filhos[0]) == 0)
+                for (int i = 0; i <= arv->tamanho; i++) {
+                    printArv(arv->filhos[i], altura+1);
                 }
             altura++;
         }
     }
 }
 
-void printVetor(Arvb *arv){//printa e forma de vetor
-    if(vaziaOrdem(arv)){
+void printVetor(Arvb *arv) { // Função ñ utilizada
+    if (vaziaOrdem(arv)) {
         printf("nao inicializada\n");
-    }else{
-        if(vazia(arv)){
+    } else {
+        if (vazia(arv)) {
             printf("sem elementos\n");
-        }else{
-            printLista(arv->itens,arv->tamanho);
+        } else {
+            printLista(arv->itens, arv->tamanho);
             // if(arv==raiz)
             //     printf("\n");
-            if(vaziaOrdem(arv->filhos[0])==0){
-                for(int i=0;i<=arv->tamanho;i++){
+            if (vaziaOrdem(arv->filhos[0]) == 0) {
+                for (int i = 0; i<= arv->tamanho; i++) {
                     printVetor(arv->filhos[i]);
                 }
             }
@@ -440,12 +418,11 @@ void printVetor(Arvb *arv){//printa e forma de vetor
 
 #pragma endregion
 
+#pragma region Função Menu
 
-#pragma region  Menu
-
-void menu(int *b){
-    char escolha='0';
-    while(escolha!='N'){
+void menu(int *b) {
+    char escolha = '0';
+    while (escolha != 'N') {
         printf("Escolha uma opcão\n");
         printf("1-inserir\n");
         printf("2-inserir aleatorio\n");
@@ -455,36 +432,36 @@ void menu(int *b){
         printf("6-limpar a tela\n");
         printf("7-apagar arvore\n");
         printf("8-sair\n");
-        scanf("%c",&escolha);
-        switch(escolha){
+        scanf("%c", &escolha);
+        switch (escolha) {
             case '1':
                 printf("digite um numero\n");
                 int numero;
-                scanf("%d",&numero);
-                inserir(raiz,NULL,numero);
+                scanf("%d", &numero);
+                inserir(raiz, NULL, numero);
                 break;
             case '2':
                 printf("digite a quantidade de numeros\n");
                 int quantidade;
-                scanf("%d",&quantidade);
+                scanf("%d", &quantidade);
                 srand(time(NULL));
-                for(int i=1;i<quantidade;i++){
-                    inserir(raiz,NULL,rand()%(i*10));
+                for (int i = 1; i < quantidade; i++) {
+                    inserir(raiz, NULL, rand()%(i*10));
                 }
                 break;
-            case '3':printArv(raiz,0);
+            case '3':printArv(raiz, 0);
                 break;
             case '4':
                 printf("Digite o numero que quer pesquisar\n");
                 int pesquisar;
-                scanf("%d",&pesquisar);
-                pesquisa(raiz,pesquisar);
+                scanf("%d", &pesquisar);
+                pesquisa(raiz, pesquisar);
                 break;
             case '5': 
                 printf("Digite o item para ser removido\n");
                 int valor;
-                scanf("%d",&valor);
-                removerArv(raiz,NULL,valor);
+                scanf("%d", &valor);
+                removerArv(raiz, NULL, valor);
                 break;
             case '6': system("clear"); break;
             case '7': apagar(raiz); break;
@@ -495,9 +472,9 @@ void menu(int *b){
             break;
             default:
                 printf("Opcao invalida\n");
-                scanf("%c",&escolha);
+                scanf("%c", &escolha);
         }
-        scanf("%c",&escolha);
+        scanf("%c", &escolha);
     }
 }
 
